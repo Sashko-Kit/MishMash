117f5b26252649637ee20837fe884588c34b7664
SashkoKit <suskakit@gmail.com>
2024-04-23 18:44:36 +0300

    The addition of our first item: Rice Pudding!

---
+++ src/main/java/net/sash/mish_mash/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.sash.mish_mash.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sash.mish_mash.MishMash;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MishMash.MOD_ID);
+
+    public static final RegistryObject<Item> RICE_PUDDING = ITEMS.register("rice_pudding",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
